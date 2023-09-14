261852853e3dbd1508244f7cda7442237e4f4b5b
saranyasneha <saranya@entrans.io>
2023-09-14 22:45:47 +0530

    login and register api

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-console.log("hi helloo world");
+const express = require("express");
+const cors = require("cors");
+const uuid = require("uuid");
+const bcrypt = require('bcrypt');
+require("dotenv").config();
+const db = require("./config/database");
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req, res) => {
+    try {
+        db.query('SELECT * FROM users', (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            res.json(results)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+});
+app.post("/register", async (req, res) => {
+    try {
+        const { FirstName, LastName, Email, Password } = req.body;
+        const userId = uuid.v4();
+        const myEncPassword = await bcrypt.hash(Password, 10);
+        db.query('SELECT * FROM users WHERE Email=?', [Email], (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: "Internal server error" })
+            }
+            if (results.length > 0) {
+                return res.status(400).json({ error: "User already exists!!" })
+            }
+            db.query('INSERT INTO users(ID,FirstName,LastName,Email,Password) VALUES (?,?,?,?,?)', [userId, FirstName, LastName, Email, myEncPassword], (err, results) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({ error: "Internal server error" })
+                }
+                const users = {
+                    ID: userId,
+                    FirstName,
+                    LastName,
+                    Email,
+                    Password: myEncPassword
+                }
+                return res.status(201).json({ message: "User registered successfully", user: users })
+            })
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+app.post("/login", (req, res) => {
+    try {
+        const { Email, Password } = req.body;
+        console.log(Email);
+        db.query('SELECT * FROM users WHERE Email=? ', [Email], async (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+            console.log(results);
+            if (results.length == 0) {
+                return res.status(401).json({ error: 'Invalid credentials' })
+            }
+            const user = results[0];
+            const hashedPassword = user.Password;
+            const passwordMatch = await bcrypt.compare(Password, hashedPassword);
+            if (passwordMatch) {
+                return res.status(200).json({ message: "Login successful" })
+            } else {
+                return res.status(200).json({ message: "Invalid credentials" })
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+app.listen(4000, () => {
+    console.log(`server is running on port 4000`);
+})
